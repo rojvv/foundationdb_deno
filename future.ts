867e220287dda30e7f3b94273050f69ad9d98f2d
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2023-03-22 10:39:54 +0200

    Fix memory leak in Future constructor (#4)

--- future.ts
+++ future.ts
@@ -1,20 +1,30 @@
 import { lib } from "./lib.ts";
 import { checkFDBErr, PointerContainer } from "./utils.ts";
 
+const FUTURE_CB_MAP = new Map<number | bigint, Future>();
+const FUTURE_CALLBACK = new Deno.UnsafeCallback(
+  { parameters: ["pointer", "pointer"], result: "void" },
+  (pointer) => {
+    if (!pointer) {
+      return;
+    }
+    const pointerValue = Deno.UnsafePointer.value(pointer)
+    const future = FUTURE_CB_MAP.get(pointerValue);
+    if (future) {
+      FUTURE_CB_MAP.delete(pointerValue);
+      future.callback?.(future);
+    }
+  },
+);
+
 export class Future {
   private callback?: (future: Future) => void;
 
   constructor(private pointer: NonNullable<Deno.PointerValue>) {
+    FUTURE_CB_MAP.set(Deno.UnsafePointer.value(this.pointer), this);
     checkFDBErr(lib.fdb_future_set_callback(
       this.pointer,
-      new Deno.UnsafeCallback(
-        { parameters: ["pointer", "pointer"], result: "void" },
-        (pointer) => {
-          if (pointer) {
-            this.callback?.(this);
-          }
-        },
-      ).pointer,
+      FUTURE_CALLBACK.pointer,
       null,
     ));
   }
