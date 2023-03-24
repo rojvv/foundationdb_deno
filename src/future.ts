c3f1b35e11bfad8fd5a88e3a74462be1a0b92831
Roj <ez@roj.im>
2023-03-24 20:29:05 +0300

    Make FUTURE_CB_MAP and FUTURE_CALLBACK static properties

--- src/future.ts
+++ src/future.ts
@@ -1,30 +1,33 @@
 import { lib } from "./lib.ts";
 import { checkFDBErr, PointerContainer } from "./utils.ts";
 
-const FUTURE_CB_MAP = new Map<number | bigint, Future>();
-const FUTURE_CALLBACK = new Deno.UnsafeCallback(
-  { parameters: ["pointer", "pointer"], result: "void" },
-  (pointer) => {
-    if (!pointer) {
-      return;
-    }
-    const pointerValue = Deno.UnsafePointer.value(pointer)
-    const future = FUTURE_CB_MAP.get(pointerValue);
-    if (future) {
-      FUTURE_CB_MAP.delete(pointerValue);
-      future.callback?.(future);
-    }
-  },
-);
-
 export class Future {
+  private static FUTURE_CALLBACK_MAP = new Map<number | bigint, Future>();
+  private static FUTURE_CALLBACK = new Deno.UnsafeCallback(
+    { parameters: ["pointer", "pointer"], result: "void" },
+    (pointer) => {
+      if (!pointer) {
+        return;
+      }
+      const pointerValue = Deno.UnsafePointer.value(pointer);
+      const future = Future.FUTURE_CALLBACK_MAP.get(pointerValue);
+      if (future) {
+        Future.FUTURE_CALLBACK_MAP.delete(pointerValue);
+        future.callback?.(future);
+      }
+    },
+  );
+
   private callback?: (future: Future) => void;
 
   constructor(private pointer: NonNullable<Deno.PointerValue>) {
-    FUTURE_CB_MAP.set(Deno.UnsafePointer.value(this.pointer), this);
+    Future.FUTURE_CALLBACK_MAP.set(
+      Deno.UnsafePointer.value(this.pointer),
+      this,
+    );
     checkFDBErr(lib.fdb_future_set_callback(
       this.pointer,
-      FUTURE_CALLBACK.pointer,
+      Future.FUTURE_CALLBACK.pointer,
       null,
     ));
   }
@@ -47,15 +50,19 @@ export class Future {
 
   getValue() {
     const alloc = new ArrayBuffer(16);
-    const outValueContainer = new PointerContainer(new BigUint64Array(alloc, 0, 1));
+    const outValueContainer = new PointerContainer(
+      new BigUint64Array(alloc, 0, 1),
+    );
     const outPresentAndLengthBuffer = new Uint32Array(alloc, 8, 2);
     const outPresentPointer = Deno.UnsafePointer.of(outPresentAndLengthBuffer);
-    const outLengthPointer = Deno.UnsafePointer.of(outPresentAndLengthBuffer.subarray(1));
+    const outLengthPointer = Deno.UnsafePointer.of(
+      outPresentAndLengthBuffer.subarray(1),
+    );
     checkFDBErr(lib.fdb_future_get_value(
       this.pointer,
       outPresentPointer,
       outValueContainer.use(),
-      outLengthPointer
+      outLengthPointer,
     ));
     const outPresent = outPresentAndLengthBuffer[0] !== 0;
     if (!outPresent) {
